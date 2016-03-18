0755b3004dba07ee1a256de4669498f1ae78a21d
chalmie <blairchalmers@gmail.com>
2016-03-18 10:10:58 -0700

    add README

--- js/repo-interface.js
+++ js/repo-interface.js
@@ -1,14 +1,14 @@
-var apiKey = require('./../.env').apiKey;
-var getRepos = require('./../js/repo.js').getRepos;
-
-$(document).ready(function(){
-  $('#repos').click(function(){
-    var username = $('#username').val();
-    $('#username').val("");
-    $('.user').text(username);
-    getRepos(username);
-
-
-
-  });
-});
+// var apiKey = require('./../.env').apiKey;
+// var getRepos = require('./../js/repo.js').getRepos;
+//
+// $(document).ready(function(){
+//   $('#repos').click(function(){
+//     var username = $('#username').val();
+//     $('#username').val("");
+//     $('.user').text(username);
+//     getRepos(username);
+//
+//
+//
+//   });
+// });
--- js/repo.js
+++ js/repo.js
@@ -1,19 +1,19 @@
-var apiKey = require('./../.env').apiKey;
-
-
-exports.getRepos = function(username){
-  $.get('https://api.github.com/users/' + username +'/repos?access_token=' + apiKey).then(function(response) {
-    i = 0;
-    while (i < response.length) {
-      if(response[i].description === "") {
-        $('.show-repos').append('<p>' + response[i].name + ' - ' + '[description not provided]' + '</p>');
-      } else {
-        $('.show-repos').append('<p>' + response[i].name + ' - ' + response[i].description + '</p>');
-      }
-    i++;
-    }
-  }).fail(function(error) {
-    console.log(error.responseJSON.message);
-  });
-
-};
+// var apiKey = require('./../.env').apiKey;
+//
+//
+// exports.getRepos = function(username){
+//   $.get('https://api.github.com/users/' + username +'/repos?access_token=' + apiKey).then(function(response) {
+//     i = 0;
+//     while (i < response.length) {
+//       if(response[i].description === "") {
+//         $('.show-repos').append('<p>' + response[i].name + ' - ' + '[description not provided]' + '</p>');
+//       } else {
+//         $('.show-repos').append('<p>' + response[i].name + ' - ' + response[i].description + '</p>');
+//       }
+//     i++;
+//     }
+//   }).fail(function(error) {
+//     console.log(error.responseJSON.message);
+//   });
+//
+// };
